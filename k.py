ff1f153e204be8c7b20cffed77b51dbb13469cb8
NorahSaad <143969550+NorahSaad@users.noreply.github.com>
2023-09-10 00:50:20 +0300

    Add files via upload

---
+++ k.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  #insert and print data set using pandas library
+
+# In[20]:
+
+
+import pandas as pd 
+
+
+# In[21]:
+
+
+
+pd= pandas.read_csv('cost-revenue-clean.csv')
+
+
+# In[22]:
+
+
+pd.describe()
+
+
+# In[ ]:
+
+
+
+
